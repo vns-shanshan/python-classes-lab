860c21d3f1f82bb33a8d60cd197188801780243a
huijingj <vanessajin0426@gmail.com>
2024-10-23 11:14:14 -0700

    Finished tic tac toe game.

---
+++ exercises.py
@@ -0,0 +1,90 @@
+class Game():
+    board = {
+        'a1': None, 'b1': None, 'c1': None,
+        'a2': None, 'b2': None, 'c2': None,
+        'a3': None, 'b3': None, 'c3': None,
+    }
+
+    def __init__(self, turn="X", tie=False, winner=None, board=board):
+        self.turn = turn
+        self.tie = tie
+        self.winner = winner
+        self.board = board
+
+    def print_board(self):
+        b = self.board
+        print(f"""
+                A   B   C
+            1)  {b['a1'] or ' '} | {b['b1'] or ' '} | {b['c1'] or ' '}
+                ----------
+            2)  {b['a2'] or ' '} | {b['b2'] or ' '} | {b['c2'] or ' '}
+                ----------
+            3)  {b['a3'] or ' '} | {b['b3'] or ' '} | {b['c3'] or ' '}
+            """)
+
+    def print_message(self):
+        if self.tie:
+            print("Tie game!")
+        elif self.winner:
+            print(f"{self.winner} wins the game!")
+        else:
+            print(f"It's a player {self.turn}'s turn!")
+    
+    def render(self):
+        self.print_board()
+        self.print_message()
+
+    def get_move(self):
+        while True:
+            move = input(f"Enter a valid move (example: A1): ").lower()
+
+            if move in self.board and self.board[move] == None:
+                return move
+            else:
+                print("Invalid input. Please try again.")
+
+    def check_for_winner(self):
+        win_combinations = [
+            ["a1", "b1", "c1"], ["a2", "b2", "c2"], ["a3", "b3", "c3"],
+            ["a1", "a2", "a3"], ["b1", "b2", "b3"], ["c1", "c2", "c3"],
+            ["a1", "b2", "c3"], ["a3", "b2", "c1"],
+        ]
+
+        for combo in win_combinations:
+            if self.board[combo[0]] and self.board[combo[0]] == self.board[combo[1]] == self.board[combo[2]]:
+                self.winner = self.board[combo[0]]
+                return
+            
+    def check_for_tie(self):
+        is_board_filled = True
+
+        for key, value in self.board.items():
+            if value == None:
+                is_board_filled = False
+                break
+        
+        if is_board_filled and self.winner == None:
+            self.tie = True
+    
+    def switch_turn(self):
+        if self.turn == "O":
+            self.turn = "X"
+        else:
+            self.turn = "O"
+
+    def play_game(self):
+        print("Welcome to tic tac toe game!")
+
+        while not self.winner and not self.tie:
+            self.render()
+            move = self.get_move()
+            self.board[move] = self.turn
+            self.check_for_winner()
+            self.check_for_tie()
+            if not self.winner and not self.tie:
+                self.switch_turn()
+        
+        self.render()
+
+game_instance = Game()
+game_instance.play_game()
